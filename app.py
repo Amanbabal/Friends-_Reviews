797d00d467713c4dc02995d026e5102a2aaae569
Aman Babal <babalaman529@gmail.com>
2026-04-20 15:08:11 +0530

    final

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def data():
     return render_template("data.html", rows=rows)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port= 5432, debug=True)
+    app.run(host="0.0.0.0", port= 5432)
 
